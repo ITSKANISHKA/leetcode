9cf3d4c87594d563d70b971d44ce0af6eba9ba99
ITSKANISHKA <142567330+ITSKANISHKA@users.noreply.github.com>
2026-01-07 19:58:23 +0530

    Time: 3 ms (96.54%) | Memory: 47.2 MB (34.48%) - LeetSync

--- 72-edit-distance/edit-distance.java
+++ 72-edit-distance/edit-distance.java
@@ -1,25 +1,25 @@
 class Solution {
     public int minDistance(String word1, String word2) {
-        int n = word1.length();
-        int m = word2.length();
-        int[][] dp = new int[n+1][m+1];
-        for(int i=0; i<=n; i++) Arrays.fill(dp[i], -1);
-        return solve(word1, word2, 0, 0, dp);
-    }
-    private int solve(String s, String t, int i, int j, int[][] dp) {
-        if(i == s.length()) return t.length() - j;
-        if(j == t.length()) return s.length() - i; 
-
-        if(dp[i][j] != -1) return dp[i][j];
-
-        if(s.charAt(i) == t.charAt(j)) {
-            dp[i][j] = solve(s, t, i+1, j+1, dp);
-        } else {
-            int insert = 1 + solve(s, t, i, j+1, dp);
-            int delete = 1 + solve(s, t, i+1, j, dp);
-            int replace = 1 + solve(s, t, i+1, j+1, dp);
-            dp[i][j] = Math.min(insert, Math.min(delete, replace));
+        int n=word1.length();
+        int m=word2.length();
+        int[][] dp=new int[word1.length()+1][word2.length()+1];
+        for(int[] num:dp){
+            Arrays.fill(num,-1);
         }
-        return dp[i][j];
+        return Solve(word1,word2,0,0,dp);
+    }
+    public static int Solve(String s,String t,int i,int j,int[][] dp){
+        if(i==s.length())return t.length()-j;
+        if(j==t.length())return s.length()-i;
+        if(dp[i][j]!=-1)return dp[i][j];
+        int ans=0;
+        if(s.charAt(i)==t.charAt(j)){
+            ans=Solve(s,t,i+1,j+1,dp);
+        }else{
+            int d=Solve(s,t,i+1,j,dp);
+            int in=Solve(s,t,i,j+1,dp);
+            int r=Solve(s,t,i+1,j+1,dp);
+            ans=1+Math.min(d,Math.min(in,r));
+        }return dp[i][j]=ans;
     }
 }
