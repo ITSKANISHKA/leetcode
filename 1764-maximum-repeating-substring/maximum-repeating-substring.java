608cafe39991210d4a5b35d197b2ee7cfe4669f0
ITSKANISHKA <142567330+ITSKANISHKA@users.noreply.github.com>
2026-01-08 10:21:18 +0530

    Time: 1 ms (83.48%) | Memory: 43.3 MB (12.68%) - LeetSync

--- 1764-maximum-repeating-substring/maximum-repeating-substring.java
+++ 1764-maximum-repeating-substring/maximum-repeating-substring.java
@@ -1,11 +1,22 @@
 class Solution {
     public int maxRepeating(String sequence, String word) {
-        int c=0;
-        String tar = word;
-        while(sequence.contains(tar)){
-            c++;
-            tar = tar+word;
+        int[] dp = new int[sequence.length()];
+        Arrays.fill(dp, -1);
+        int ans = 0;
+        for (int i = 0; i < sequence.length(); i++) {
+            ans = Math.max(ans, solve(sequence, word, i, dp));
         }
-        return c;
+        return ans;
+    }
+    public static int solve(String s, String w, int i, int[] dp) {
+        if (i + w.length() > s.length()) return 0;
+        if (dp[i]!=-1) return dp[i];
+
+        if (s.startsWith(w, i)) {
+            dp[i]=1+solve(s,w,i +w.length(),dp);
+        } else {
+            dp[i]=0;
+        }
+        return dp[i];
     }
 }
