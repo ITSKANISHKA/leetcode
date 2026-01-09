11fd543167d6d4bdc15eb1d682747f2134b349ab
ITSKANISHKA <142567330+ITSKANISHKA@users.noreply.github.com>
2026-01-09 11:53:31 +0530

    Time: 3 ms (95.82%) | Memory: 46 MB (72.33%) - LeetSync

---
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.java
@@ -0,0 +1,33 @@
+class Solution {
+
+    public int lengthOfLIS(int[] arr) {
+        if (arr == null || arr.length == 0) return 0;
+
+        int[] dp = new int[arr.length];
+        int len = 1;
+        dp[0] = arr[0];
+
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[i] > dp[len - 1]) {
+                dp[len] = arr[i];
+                len++;
+            } else {
+                int idx = lowerBound(dp, 0, len - 1, arr[i]);
+                dp[idx] = arr[i];
+            }
+        }
+        return len;
+    }
+
+    private static int lowerBound(int[] dp, int si, int ei, int item) {
+        while (si <= ei) {
+            int mid = si + (ei - si) / 2;
+            if (dp[mid] >= item) {
+                ei = mid - 1;
+            } else {
+                si = mid + 1;
+            }
+        }
+        return si;
+    }
+}
