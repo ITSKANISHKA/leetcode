1adb8721aad2e5321894f2eecb07b3d53d57cf32
ITSKANISHKA <142567330+ITSKANISHKA@users.noreply.github.com>
2026-02-06 12:48:11 +0530

    Time: 79 ms (7.60%) | Memory: 90.4 MB (20.40%) - LeetSync

--- 3751-maximum-frequency-after-subarray-operation/maximum-frequency-after-subarray-operation.java
+++ 3751-maximum-frequency-after-subarray-operation/maximum-frequency-after-subarray-operation.java
@@ -1,4 +1,3 @@
-
 class Solution {
     public static int solve(int[] nums, int k) {
         int k_freq = 0;
@@ -6,13 +5,10 @@ class Solution {
         for (int i : nums) {
             map.put(i, map.getOrDefault(i, 0) + 1);
         }
-        
         if (map.containsKey(k)) {
             k_freq += map.get(k);
-        }
-        
+        }  
         int max = 0;
-        
         for (int v = 1; v <= 50; v++) {
             if (v == k) continue;            
             int curr_sum = 0;
@@ -23,25 +19,20 @@ class Solution {
                 } else if (num == k) {
                     curr_sum -= 1;
                 }
-
                 if (curr_sum < 0) {
                     curr_sum = 0;
                 }
-
                 if (curr_sum > temp_max) {
                     temp_max = curr_sum;
                 }
             }
-
             if (temp_max > max) {
                 max = temp_max;
             }
         }
-
         int maxfreq = k_freq + max;
         return maxfreq;
     }
-
     public int maxFrequency(int[] nums, int k) {
         return solve(nums, k);
     }
