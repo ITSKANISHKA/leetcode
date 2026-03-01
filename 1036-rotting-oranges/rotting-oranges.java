cbe2b583362ca851c3d5bf975accadd4383c0f9e
ITSKANISHKA <142567330+ITSKANISHKA@users.noreply.github.com>
2026-03-01 20:59:52 +0530

    Time: 2 ms (91.16%) | Memory: 44 MB (85.15%) - LeetSync

--- 1036-rotting-oranges/rotting-oranges.java
+++ 1036-rotting-oranges/rotting-oranges.java
@@ -1,35 +1,61 @@
+class Pair{
+    int row;
+    int col;
+    int time;
+    Pair(int row,int col,int time){
+        this.row=row;
+        this.col=col;
+        this.time=time;
+    }
+}
+
 class Solution {
     public int orangesRotting(int[][] grid) {
-        int n=grid.length;
-        if(grid==null || n==0)return -1;
-        for(int i=0;i<n;i++){
-            for(int j=0;j<grid[0].length;j++){
-                if(grid[i][j]==2){
-                    dfs(grid,i,j,2);
+       Queue<Pair> q=new LinkedList<Pair>();
+       int n=grid.length;
+       int m=grid[0].length;
+       int count=0;
+       int maxTime=0;
+       int[][] visited=new int[n][m];
+       for(int i=0;i<n;i++){
+        for(int j=0;j<m;j++){
+            if(grid[i][j]==2){
+                q.add(new Pair(i,j,0));
+                visited[i][j]=2;
+            }
+            else{
+                visited[i][j]=0;
+            }
+            if(grid[i][j]==1)count++;
+        }
+       }
+
+       int[] dRow = {1, 0, -1, 0};
+        int[] dCol = {0, 1, 0, -1};
+
+        // Step 2: BFS
+        while(!q.isEmpty()) {
+            Pair cur = q.poll();
+            int r = cur.row;
+            int c = cur.col;
+            int t = cur.time;
+
+            maxTime = Math.max(maxTime, t);
+
+            for(int k = 0; k < 4; k++) {
+                int nr = r + dRow[k];
+                int nc = c + dCol[k];
+
+                if(nr >= 0 && nc >= 0 && nr < n && nc < m &&
+                   visited[nr][nc] == 0 && grid[nr][nc] == 1) {
+
+                    visited[nr][nc] = 2;
+                    count--;
+                    q.add(new Pair(nr, nc, t + 1));
                 }
             }
         }
-        int ans=2;
-        for(int[] row:grid){
-            for(int col:row){
-                if(col==1)return -1;
-                ans=Math.max(ans,col);
-            }
-        }return ans-2;
-        
-    }
-    public static void dfs(int[][] grid,int i,int j,int ans){
-        
-        if(i>=grid.length || i<0 || j<0||j>=grid[0].length || grid[i][j]==0 || (grid[i][j]>1 && grid[i][j]<ans))return;
-        else{
-
-        
-        grid[i][j]=ans;
-        
-            dfs(grid,i+1,j,ans+1);
-            dfs(grid,i,j+1,ans+1);
-            dfs(grid,i-1,j,ans+1);
-            dfs(grid,i,j-1,ans+1);
-        }
+        if(count > 0) return -1;
+        return maxTime;
     }
 }
