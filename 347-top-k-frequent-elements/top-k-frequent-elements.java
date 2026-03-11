5bc422144fcb0010098c0c3f556b99d7b62d7dd4
ITSKANISHKA <142567330+ITSKANISHKA@users.noreply.github.com>
2026-03-11 12:23:54 +0530

    Time: 16 ms (39.39%) | Memory: 47 MB (99.27%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.java
+++ 347-top-k-frequent-elements/top-k-frequent-elements.java
@@ -5,7 +5,7 @@ class Solution {
         for(int i=0;i<n;i++){
             map.put(nums[i],map.getOrDefault(nums[i],0)+1);
         }
-        PriorityQueue<Integer> pq=new PriorityQueue<>((a,b) -> map.get(a) - map.get(b));;
+        PriorityQueue<Integer> pq=new PriorityQueue<>((a,b) -> map.get(a) - map.get(b));
         for(int num : map.keySet()) {
             pq.offer(num);
             if(pq.size() > k) {
