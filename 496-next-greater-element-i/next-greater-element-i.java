18fac56544e617a1c946d22b3d63992a7921a907
ITSKANISHKA <142567330+ITSKANISHKA@users.noreply.github.com>
2026-05-30 00:59:37 +0530

    Time: 4 ms (75.77%) | Memory: 45.4 MB (35.51%) - LeetSync

--- 496-next-greater-element-i/next-greater-element-i.java
+++ 496-next-greater-element-i/next-greater-element-i.java
@@ -1,17 +1,17 @@
 class Solution {
-    public int[] nextGreaterElement(int[] nums1, int[] nums2) {
-        Map<Integer, Integer> ng = new HashMap<>();
-        Deque<Integer> st = new ArrayDeque<>();
+    public int[] nextGreaterElement(int[] nums1,int[] nums2) {
+        Map<Integer, Integer> map = new HashMap<>();
+        Stack<Integer> st = new Stack<>();
         for (int num : nums2) {
-            while (!st.isEmpty() && st.peek() < num) {
-                ng.put(st.pop(), num);
+            while (!st.isEmpty() && num > st.peek()) {
+                map.put(st.pop(), num);
             }
             st.push(num);
         }
-        int[] res = new int[nums1.length];
+        int[] result = new int[nums1.length];
         for (int i = 0; i < nums1.length; i++) {
-            res[i] = ng.getOrDefault(nums1[i], -1);
+            result[i] = map.getOrDefault(nums1[i],-1);
         }
-        return res;        
+        return result;
     }
 }
